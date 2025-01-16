c560b71ed76f7083228991e90b19d854c0d2a221
maynealis <mayneclara@gmail.com>
2025-01-16 22:55:02 +0100

    printf: done mandatory part

--- Printf/ft_printf.c
+++ Printf/ft_printf.c
@@ -14,60 +14,66 @@
 #include "libft.h"
 #include "ft_utils.h"
 
-#define CONVERSIONS "cspdiuxX%"
+//#define CONVERSIONS "cspdiuxX%"
 
-int	write_subst(char type, void *sub)
+// int	write_subst_old(char type, void *sub)
+// {
+// 	if (type == 'c')
+// 		return (write_char(*((char *)sub)));
+// 	else if (type == '%')
+// 		return (write_char('%'));
+// 	else if (type == 's')
+// 		return (write_str((char *)sub));
+// 	else if (type == 'p')
+// 		return (write_hexa((unsigned long)sub, 'x'));
+// 	else if (type == 'd' || type == 'i')
+// 		return (write_int(*((int *)sub)));
+// 	else if (type == 'u')
+// 		return (write_uint(*((unsigned int *)sub)));
+// 	else if (type == 'x' || type == 'X')
+// 		return (write_hexa(*((unsigned int *)sub), type));
+// 	return (-1); //error??
+// }
+
+int	write_subst(char type, va_list args)
 {
-	if (type == 'c' || type == '%')
-		return (write_char(*((char *)sub)));
+	if (type == 'c')
+		return (write_char(va_arg(args, int))); // TODO: why then char is actually an int?
+	else if (type == '%')
+		return (write_char('%'));
 	else if (type == 's')
-		return (write_str((char *)sub));
+		return (write_str(va_arg(args, char *)));
 	else if (type == 'p')
-		return (write_hexa((unsigned long)sub, 'x'));
+		return (write_hexa(va_arg(args, unsigned long), 'x'));
 	else if (type == 'd' || type == 'i')
-		return (write_int(*((int *)sub)));
+		return (write_int(va_arg(args, int)));
 	else if (type == 'u')
-		return (write_uint(*((unsigned int *)sub)));
+		return (write_uint(va_arg(args, unsigned int)));
 	else if (type == 'x' || type == 'X')
-		return (write_hexa(*((unsigned int *)sub), type));
+		return (write_hexa(va_arg(args, unsigned long), type));
 	return (-1); //error??
 }
 
 int	ft_printf(const char *str, ...)
 {
 	int		result;
-	va_list args;
+	va_list	args;
+	size_t	i;
 
 	va_start(args, str);
-
-	char	*subst;
-	subst = ft_strchr(str, '%');
-
-	// Nothing to substitute
-	if (subst == NULL)
-	{
-		ft_putstr_fd((char *)str, 1);
-		va_end(args);
-		return (ft_strlen(str));
-	}
 	result = 0;
-	int	ini = 0;
-	while (subst)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		int		len = (int)(subst - (str + ini));
-		char	*to_write = ft_substr(str, ini, len)); //malloc
-		result += write_str(to_write);
-		free(to_write);
-
-		result += write_subst(*(subst++), va_arg(args, void *));
-		
-		ini = (int)subst + 1;
-		subst = ft_strchr(++subst, '%');
-	}
-	if (subst != NULL)
-	{
-		result += write_str(subst);
+		if (str[i] == '%')
+		{
+			i++; //conversion
+			result += write_subst(str[i], args);
+		}
+		else
+			result += write_char(str[i]);
+		i++;
 	}
 	va_end(args);
-	return (1);
+	return (result);
 }
--- Printf/tests/main.c
+++ Printf/tests/main.c
@@ -11,11 +11,41 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
 int	main(void)
-{
-//	ft_printf("Simple print\n");
+{	
+	int	my_result;
+	int	or_result;
+
+	// Without arguments
+	ft_printf("################### `TAB Simple print` ###################\n");
+	my_result = ft_printf("	Simple print\n");
+	or_result = printf("	Simple print\n");
+	ft_printf("\nresult: %d - %d\n", my_result, or_result);
+	ft_printf("##########################################################\n");
+
+	// With single character substitution
+	ft_printf("######################## char: `c` ########################\n");
+	my_result = ft_printf("char: %c", 'c');
+	or_result = printf("char: %c", 'c');
+	ft_printf("\nresult: %d - %d\n", my_result, or_result);
+	ft_printf("-----------------------------------------------------------\n");
+	ft_printf("###################### char: `\\n` ########################\n");
+	my_result = ft_printf("%c", '\n');
+	or_result = printf("%c", '\n');
+	ft_printf("\nresult: %d - %d\n", my_result, or_result);
+	ft_printf("##########################################################\n");
+
+	// With multiple character substitution
+
+	// With single string substitution
+
+	// With multiple string substitution
 	ft_printf("Print with substitution at the end %s", "---this is the substitution\n");
-//	ft_printf("Print with %s at the middle %s", "---this is the substitution---");
+	ft_printf("Print with %s at the middle %s\n", "---this is the substitution---");
+	ft_printf("Multiple subs: %s, and here goes: %c", "---first---", 'c');
+	ft_printf("A integer: %d\n another one: %i\n", 42, 90765);
+	ft_printf("hexa: %x\n", 32);
 	return (0);
 }
